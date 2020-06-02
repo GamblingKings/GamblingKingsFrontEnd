aa66b8e68725a5d34cb64774bde2e6bab4440141
Derrick Lee <gorick2491@hotmail.com>
2020-06-02 10:57:27 -0700

    Add class methods and add/remove listeners methods

--- src/__tests__/WebSocket.test.ts
+++ src/__tests__/WebSocket.test.ts
@@ -2,19 +2,80 @@ import WS from 'jest-websocket-mock';
 
 import WebSocketConnection from '../modules/ws/websocket';
 
-test('Mock ws connection', async () => {
+const key = 'TEST_MESSAGE';
+const invalidKey = 'INVALID_KEY';
+const data = { test_message: 'hello world', test_message2: 'hello' };
+
+const json = JSON.stringify({ action: key, ...data });
+const invalidJSON = JSON.stringify({ action: invalidKey, ...data });
+
+test('Mock WS connection: client send server message', async () => {
+  WS.clean();
   const fakeURL = 'ws://localhost:1234';
   const mockServer = new WS(fakeURL);
   // await mockServer.connected;
 
   const wsConnection = new WebSocketConnection(fakeURL);
-  const key = 'TEST_MESSAGE';
-  const data = { test_message: 'hello world', test_message2: 'hello' };
+
   wsConnection.sendMessage(key, data);
-  const json = JSON.stringify({ action: key, ...data });
+  wsConnection.sendMessage(invalidKey, data);
 
   await expect(mockServer).toReceiveMessage(json);
   expect(mockServer).toHaveReceivedMessages([json]);
 
   WS.clean();
+});
+
+test('Mock WS Connection: Add listener and receive message', async () => {
+  WS.clean();
+  const fakeURL = 'ws://localhost:1234';
+  const mockServer = new WS(fakeURL);
+  // await mockServer.connected;
+
+  const wsConnection = new WebSocketConnection(fakeURL);
+
+  const message: string[] = [];
+  const callback = (mockData: JSON) => {
+    const jsonString = JSON.stringify(mockData);
+    message.push(jsonString);
+  };
+
+  const listenerAdded = wsConnection.addListener(key, callback);
+  const sameListenerAddedInError = wsConnection.addListener(key, callback);
+
+  expect(listenerAdded).toBeTruthy();
+  expect(sameListenerAddedInError).toBeFalsy();
+
+  mockServer.send(json);
+  mockServer.send(invalidJSON);
+
+  expect(message).toEqual([json]);
+});
+
+test('Mock WS Connection: Add/Remove listener', async () => {
+  WS.clean();
+  const fakeURL = 'ws://localhost:1234';
+  const mockServer = new WS(fakeURL);
+  // await mockServer.connected;
+
+  const wsConnection = new WebSocketConnection(fakeURL);
+
+  const message: string[] = [];
+  const callback = (mockData: JSON) => {
+    const jsonString = JSON.stringify(mockData);
+    message.push(jsonString);
+  };
+
+  const listenerAdded = wsConnection.addListener(key, callback);
+  expect(listenerAdded).toBeTruthy();
+
+  const listenerRemoved = wsConnection.removeListener(key);
+  const listenerAlreadyRemoved = wsConnection.removeListener(key);
+
+  expect(listenerRemoved).toBeTruthy();
+  expect(listenerAlreadyRemoved).toBeFalsy();
+
+  mockServer.send(json);
+
+  expect(message).toEqual([]);
 });
--- src/modules/ws/websocket.ts
+++ src/modules/ws/websocket.ts
@@ -1,8 +1,9 @@
 import SendMessageValidator from './message_validator';
 
 interface WebSocketConnection {
-  ws: WebSocket;
   sendMessage(key: string, data: Record<string, unknown>): void;
+  addListener(key: string, callback: () => void): boolean;
+  removeListener(key: string): boolean;
 }
 
 const WEBSOCKET_URL = process.env.REACT_APP_WEBSOCKETURL || '';
@@ -11,11 +12,15 @@ const WEBSOCKET_URL = process.env.REACT_APP_WEBSOCKETURL || '';
  * Interface that the client can use for Websocket Connection.
  */
 class WebSocketConnection {
+  private ws: WebSocket;
+
+  private listeners: Record<string, (data: JSON) => void> = {};
+
   public constructor(url: string = WEBSOCKET_URL) {
     this.ws = new WebSocket(url);
     this.ws.onopen = (event) => {
       console.log(event);
-      this.ws.send('');
+      // this.ws.send('');
     };
 
     this.ws.onclose = (event) => {
@@ -23,26 +28,63 @@ class WebSocketConnection {
     };
 
     this.ws.onerror = (error) => {
+      // TODO: Build common logger
       console.log(error);
     };
 
-    this.ws.onmessage = (event) => {
-      console.log(event);
+    this.ws.onmessage = (message) => {
+      let data = null;
+      try {
+        data = JSON.parse(message.data);
+      } catch (err) {
+        console.log(err);
+        console.log('Incoming message must be a JSON.');
+        return;
+      }
+      const { action } = data;
+
+      if (action in this.listeners) this.listeners[action](data);
     };
   }
-}
 
-/**
- * Sends a message to the backend if the message is validated.
- */
-WebSocketConnection.prototype.sendMessage = function sendMessage(key: string, data: Record<string, unknown>): boolean {
-  const validMessage = SendMessageValidator.validateMessage(key, data);
-  if (validMessage) {
-    const payload = { action: key, ...data };
-    this.ws.send(JSON.stringify(payload));
+  /**
+   * Sends a message to the backend if the message is validated.
+   */
+  public sendMessage(key: string, data: Record<string, unknown>): boolean {
+    const validMessage = SendMessageValidator.validateMessage(key, data);
+    if (validMessage) {
+      const payload = { action: key, ...data };
+      this.ws.send(JSON.stringify(payload));
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Adds a listener to the WebSocketConnection so client can act upon backend messages.
+   * @param key a string, unique identifier to a callback
+   * @param callback a function that client can act on
+   * @returns a boolean whether the listener was successfully added
+   */
+  public addListener(key: string, callback: (data: JSON) => void): boolean {
+    if (key in this.listeners) return false;
+
+    this.listeners[key] = callback;
     return true;
   }
-  return false;
-};
+
+  /**
+   * Removes a listener from WebSocketConnection so the client does not need to act upon backend messages.
+   * @param key a string, unique identifier to the listener
+   * @returns a boolean whether the listener was successfully removed
+   */
+  public removeListener(key: string): boolean {
+    if (key in this.listeners) {
+      delete this.listeners[key];
+      return true;
+    }
+    return false;
+  }
+}
 
 export default WebSocketConnection;
