4c99731c222239ba53c9b2705ce50e61fec052d5
Derrick Lee <gorick2491@hotmail.com>
2020-06-05 16:22:59 -0700

    Add send_message component and handle incoming messages

---
+++ src/components/lobby/send_message.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import WebSocketConnection from '../../modules/ws/websocket';
+import OutgoingAction from '../../modules/ws/outgoing_action';
+
+type SendMessageFormProps = {
+  ws: WebSocketConnection | null;
+};
+
+/**
+ * Form Component used to send message to WebSocket to all clients.
+ */
+const SendMessageForm = ({ ws }: SendMessageFormProps): JSX.Element => {
+  const [message, setMessage] = useState<string>('');
+
+  const handleSetMessage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+  };
+
+  const sendMessage = (event: React.FormEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    const payload = {
+      message,
+    };
+
+    if (ws) {
+      ws.sendMessage(OutgoingAction.SEND_MESSAGE, payload);
+    } else {
+      // TODO: handling of client that has been disconnected from WS
+    }
+  };
+
+  return (
+    <div>
+      <p>Send Message</p>
+      <form>
+        <input value={message} onChange={handleSetMessage} />
+        <input type="submit" value="Send" onClick={sendMessage} />
+      </form>
+    </div>
+  );
+};
+
+export default SendMessageForm;
